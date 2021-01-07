a3066850f873ad75f8efb90ef2941116398c278f
nasse <nasse@192.168.2.152>
2021-01-07 12:02:09 -0500

    Probleme 2 - INCOMPLETE

---
+++ Manipulations de Chaines/src/probleme2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+/*
+* Author : nasser
+*Date : Jan. 6, 2021
+*/
+public class probleme2
+	{
+		//CETTE PROGRAME EST INCOMPLETE
+		public static void main(String[] args)
+			{
+				Scanner sc=new Scanner(System.in);
+				String sentence=sc.nextLine();
+				int cpt=0;
+				String word;
+				
+				for (String i = 0; i < sentence.length(); i++) 
+				{
+					if(i=":-)")
+						cpt=cpt++;
+						
+					
+						
+					
+				}
+				
+				
+
+				
+					
+				
+			
+			}
+			        
+
+			        
+			
+						
+		
+
+	}
